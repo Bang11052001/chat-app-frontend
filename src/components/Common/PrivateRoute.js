468e1ff76bf87fdb0cf5b1978c69c633dc4aeb16
admin <buibeo1234@gmail.con>
2022-08-23 17:04:21 +0700

    setup route

---
+++ src/components/Common/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { cookie } from "../../utils";
+import { LoginPage } from "../../features/auth/Pages";
+
+const PrivateRoute = ({ children }) => {
+  const isLogged = cookie.getCookie("access_token");
+
+  if (!isLogged) return <LoginPage />;
+  return { children };
+};
+
+export default PrivateRoute;
